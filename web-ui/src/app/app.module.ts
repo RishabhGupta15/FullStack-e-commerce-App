e677e78b623cbb77a80086a44c6ceb483e9f8796
risha <rishabhgupta15nov@gmail.com>
2022-09-29 00:24:36 +0530

    added routing

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product/product-list/product-list.component';
 import { ProductService } from './services/product.service';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+const routes: Routes = [
+  {path: "", redirectTo: "products", pathMatch: "full"},
+  {path: "add", component: ProductFormComponent},
+  {path: "products", component: ProductListComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductListComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
